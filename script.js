f15545b0e5da1efd0de0a420ab4ff824c51b9ea9
Ben Jones <benmjones@protonmail.com>
2021-09-20 18:43:07 +1200

    final fix - git is case sensitive!

--- script.js
+++ script.js
@@ -46,18 +46,18 @@ function rockGame(){
     playRound(playerSelection, computerSelection);    
     if (computerSelection == "rock") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/rock.png" alt="rock" height="100px">
+        <img class="compImage" src="/images/rock.PNG" alt="rock" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
     }
     if (computerSelection == "paper") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/paper.png" alt="paper" height="100px">
+        <img class="compImage" src="/images/paper.PNG" alt="paper" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
         computerCounter++;
     }
     if (computerSelection == "scissors") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/scissors.png" alt="scissors" height="100px">
+        <img class="compImage" src="/images/scissors.PNG" alt="scissors" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
         userCounter++;
     }
@@ -72,18 +72,18 @@ function paperGame(){
     playRound(playerSelection, computerSelection);    
     if (computerSelection == "rock") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/rock.png" alt="rock" height="100px">
+        <img class="compImage" src="/images/rock.PNG" alt="rock" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
         userCounter++;
     }
     if (computerSelection == "paper") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/paper.png" alt="paper" height="100px">
+        <img class="compImage" src="/images/paper.PNG" alt="paper" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
     }
     if (computerSelection == "scissors") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/scissors.png" alt="scissors" height="100px">
+        <img class="compImage" src="/images/scissors.PNG" alt="scissors" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
         computerCounter++;
     }
@@ -98,19 +98,19 @@ function scissorsGame(){
     playRound(playerSelection, computerSelection);    
     if (computerSelection == "rock") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/rock.png" alt="rock" height="100px">
+        <img class="compImage" src="/images/rock.PNG" alt="rock" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
         computerCounter++;
     }
     if (computerSelection == "paper") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/paper.png" alt="paper" height="100px">
+        <img class="compImage" src="/images/paper.PNG" alt="paper" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`;
         userCounter++
     }
     if (computerSelection == "scissors") {
         document.querySelector(".compChoice").innerHTML = `
-        <img class="compImage" src="/images/scissors.png" alt="scissors" height="100px">
+        <img class="compImage" src="/images/scissors.PNG" alt="scissors" height="100px">
         <h3>Score: <span class="compScore"></span></h3>`
     }
     document.querySelector(".userScore").innerText = userCounter;
